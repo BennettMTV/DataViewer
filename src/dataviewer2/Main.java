acefaa9ba1b03c0e7f01ae71051f873b8f26d130
j-mejia-ce <josh.mejia@du.edu>
2023-11-12 16:24:37 -0700

    JM_Push

--- src/dataviewer2/Main.java
+++ src/dataviewer2/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
     	String data = "data/GlobalLandTemperaturesByState.csv";
     	//String data = "data/sample.csv";
-        new DataViewerApp(data);
+        new DataViewerUI(data);
     }
 }
 
